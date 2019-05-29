3fe7a615fa8d133c5fbda56a8bb793d67ebf7b68
Muhamad Hisyam <syamcode@gmail.com>
2019-05-29 15:17:54 +0700

    created the subscribers app

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crmapp.marketing'
+    'crmapp.marketing',
+    'crmapp.subscribers'
 ]
 
 MIDDLEWARE = [
