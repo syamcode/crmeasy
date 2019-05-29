e340d2cf8df93f7dc0c0395e077a00410765b0e3
Muhamad Hisyam <syamcode@gmail.com>
2019-05-29 15:54:20 +0700

    created the subscriber view

--- crmapp/subscribers/apps.py
+++ crmapp/subscribers/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SubscribersConfig(AppConfig):
-    name = 'subscribers'
+    name = 'crmapp.subscribers'
---
+++ crmapp/subscribers/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from .forms import SubscireberForm
+
+def subscriber_new(request, template='subscibers/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscireberForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+
+            return HttpResponseRedirect('/success/')
+    else:
+        form = SubscireberForm()
+
+    return render(request, template, {'form':form})
